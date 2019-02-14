9176707024792f1b3e061972114b974871410337
Sahil Kalra <sahilkalra1991@gmail.com>
2019-02-14 23:09:03 +0100

    Increased wait timeout to 30 seconds

--- scripts/k8s_setup.sh
+++ scripts/k8s_setup.sh
@@ -6,8 +6,8 @@ set -x
 # Setup Openstack instances for k8s nodes
 ansible-playbook -i inventory/infra/hosts.ini -v infra_setup.yaml
 
-echo "Waiting for Openstack instances to boot ....."
-sleep 20
+echo "Waiting 30 seconds for Openstack instances to boot ....."
+sleep 30
 
 # Setup k8s cluster
 ansible-playbook -i inventory/cluster/hosts.ini -v cluster_setup.yaml
