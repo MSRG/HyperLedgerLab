9028156c1ff4d6593b4dc986304f676c0f408c3c
Oyku Yilmaz <oyku.yilmaz@tum.de>
2020-12-10 14:50:09 +0100

    include new configtx.yaml template
    - Include the fixed configtx.yaml (Jeeta)
    - Update endorsment policity to "ANY Readers" for tests

--- scripts/get_metrics.sh
+++ scripts/get_metrics.sh
@@ -16,5 +16,5 @@ fi
 benchmark_dir=$base_dir/$chaincode
 
 set -x
-npx caliper launch manager --caliper-bind-sut fabric:latest-v2 --caliper-benchconfig $benchmark_dir/config.yaml --caliper-networkconfig $INVENTORY_DIR_PATH/blockchain/fabric_ccp_network.yaml --caliper-fabric-gateway-enabled
+npx caliper launch manager --caliper-bind-sut fabric:latest-v2 --caliper-benchconfig $benchmark_dir/config.yaml --caliper-networkconfig $INVENTORY_DIR_PATH/blockchain/fabric_ccp_network.yaml --caliper-fabric-gateway-enabled --caliper-flow-only-test
 set +x
