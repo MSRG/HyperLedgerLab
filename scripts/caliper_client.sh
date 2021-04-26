2537a3d6dcc74d7b22f8f4efb1ff6975671f0542
jeetachacko <jeetachacko@gmail.com>
2021-04-26 06:14:52 +0000

    caliper continuous client script

---
+++ scripts/caliper_client.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Script to run benchmark for a chaincode
+# Chaincode defaults to marbles
+# Chaincode name can be provided by CLI: e.g get_metrics.sh fabcar
+
+source `dirname $0`/setup_env.sh node
+
+base_dir=$INVENTORY_DIR_PATH/blockchain/benchmark
+
+chaincode=fabcar
+if [[ ! -z $1 ]]
+then
+    chaincode=$1
+fi
+
+benchmark_dir=$base_dir/$chaincode
+
+set -x
+init=1
+while :
+do
+# loop infinitely
+if [[ $init -eq 1 ]]
+then
+	npx caliper launch manager --caliper-bind-sut fabric:latest-v2 --caliper-benchconfig $benchmark_dir/init_config.yaml --caliper-networkconfig $INVENTORY_DIR_PATH/blockchain/fabric_ccp_network.yaml --caliper-fabric-gateway-enabled --caliper-flow-only-test
+	init=0
+else
+	npx caliper launch manager --caliper-bind-sut fabric:latest-v2 --caliper-benchconfig $benchmark_dir/config.yaml --caliper-networkconfig $INVENTORY_DIR_PATH/blockchain/fabric_ccp_network.yaml --caliper-fabric-gateway-enabled --caliper-flow-only-test
+fi
+done
+set +x
